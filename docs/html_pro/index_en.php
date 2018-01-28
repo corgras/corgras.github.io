367d0e8fc92bea5a961f2b58b828a8ee9421b397
corgras <34861709+corgras@users.noreply.github.com>
2018-01-28 16:12:01 +0200

    Add files via upload

---
+++ docs/html_pro/index_en.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HTML Pro v1 - Documentation</title>
+	<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/assets/css/font-awesome.min.css" rel="stylesheet">
+	<link href="/assets/css/main.css" rel="stylesheet">
+	<link href="/assets/css/formstyle.css" rel="stylesheet">
+	<link href="/assets/css/media.css" rel="stylesheet">
+</head>
+<body>
+<div id="container">
+	<div class="rd-mobilepanel fixed">
+		<div class="logo">
+			<h1 class="rd-mobilepanel_title pull-left"><a type="button" id="button-menu" class="pull-left"><i class="fa fa-dedent fa-lg"></i></a><span>HTML Pro</span> Documentation</h1>
+			<div class="rd-mobilepanel_titler pull-right">
+				<span>Created: 31/10/2017</span>
+				<span>Latest update: 31/10/2017</span>
+				<span>Version 1</span>
+			</div>
+			<div class="mobile-button-menu">
+				<div id="menu-mobile" class="dropdown">
+					<a type="button" id="button-menu-mobile" class="dropdown-toggle" data-toggle="dropdown">Menu<i class="fa fa-dedent fa-lg"></i></a>
+					<ul class="menu-mobile-nav dropdown-menu"  role="menu" aria-labelledby="button-menu-mobile">
+						<li class="active"><a href="#home" data-toggle="tab"><i class="fa fa-home"></i><span>General Info</span></a></li>
+						<li><a href="#version" data-toggle="tab"><i class="fa fa-code-fork"></i><span>Compatibility</span></a></li>
+						<li><a href="#install" data-toggle="tab"><i class="fa fa-cogs"></i><span>Installation</span></a></li>
+						<li><a href="#setting" data-toggle="tab"><i class="fa fa-sliders"></i><span>Settings</span></a></li>
+						<li><a href="#design" data-toggle="tab"><i class="fa fa-desktop"></i><span>Design</span></a></li>
+						<li><a href="#language" data-toggle="tab"><i class="fa fa-language"></i><span>Languages</span></a></li>
+						<li><a href="#faq" data-toggle="tab"><i class="fa fa-question"></i><span>FAQ</span></a></li>
+						<li><a href="#changelog" data-toggle="tab"><i class="fa fa-history"></i><span>Changelog</span></a></li>
+						<li><a href="#contacts" data-toggle="tab"><i class="fa fa-life-ring"></i><span>Online Support</span></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<nav id="column-left">
+		<ul id="menu">
+			<li class="active"><a href="#home" data-toggle="tab"><i class="fa fa-home"></i><span>General Info</span></a></li>
+			<li><a href="#version" data-toggle="tab"><i class="fa fa-code-fork"></i><span>Compatibility</span></a></li>
+			<li><a href="#install" data-toggle="tab"><i class="fa fa-cogs"></i><span>Installation</span></a></li>
+			<li><a href="#setting" data-toggle="tab"><i class="fa fa-sliders"></i><span>Settings</span></a></li>
+			<li><a href="#design" data-toggle="tab"><i class="fa fa-desktop"></i><span>Design</span></a></li>
+			<li><a href="#language" data-toggle="tab"><i class="fa fa-language"></i><span>Languages</span></a></li>
+			<li><a href="#faq" data-toggle="tab"><i class="fa fa-question"></i><span>FAQ</span></a></li>
+			<li><a href="#changelog" data-toggle="tab"><i class="fa fa-history"></i><span>Changelog</span></a></li>
+			<li><a href="#contacts" data-toggle="tab"><i class="fa fa-life-ring"></i><span>Online Support</span></a></li>
+		</ul>
+		<div class="rd-copyright">
+			<div class="dropdown">
+				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<i class="fa fa-copyright" aria-hidden="true"></i>
+				</button>
+				<div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+					<p class="copyright">Developed by the Studio <a href="">«CORGRAS»</a> 2017</p>
+				</div>
+			</div>
+			<p class="copyright-active">Developed by the Studio <a href="">«CORGRAS»</a> © 2017 </p>
+		</div>
+	</nav>
+	<div id="content">
+		<div class="page">
+			<div id="main">
+				<div class="tab-content">
+					<section class="tab-pane active" id="home" >
+						<article class="description">
+							<h2>Introduction</h2>
+							<p>Expected</p>	
+							
+						</article>
+					</section>
+					<section class="tab-pane" id="version">
+						<article class="description version">
+							<h2>Compatibility of versions of OpenCart</h2>
+							<div class="x_title">
+								<h5>Opencart</h5>
+							</div>
+							<div class="x_content">
+								<div class="row">
+									<div class="col-md-2"><div class="box-reliz">3.0.2.0</div></div>
+									<div class="col-md-2"><div class="box-reliz">3.0.1.2</div></div>
+									<div class="col-md-2"><div class="box-reliz">3.0.1.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">3.0.0.0</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.3.0.2</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.3.0.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.3.0.0</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.2.0.0</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.2</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.0.3.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.0.2.0</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.0.1.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.0.1.0</div></div>
+								</div>
+							</div>
+							<hr>
+							<div class="x_title">
+								<h5>Opencart.pro</h5>
+							</div>
+							<div class="x_content">
+								<div class="row">
+									<div class="col-md-2"><div class="box-reliz">2.3.0.2.x</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.2.x</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.1.x</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.0.3.1</div></div>
+								</div>
+							</div>
+							<hr>
+							<div class="x_title">
+								<h5>ocStore</h5>
+							</div>
+							<div class="x_content">
+								<div class="row">
+									<div class="col-md-2"><div class="box-reliz">2.3.0.2.2</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.3.0.2.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.3.0.2</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.2.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.2</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.1.1</div></div>
+									<div class="col-md-2"><div class="box-reliz">2.1.0.1</div></div>
+								</div>
+							</div>
+						</article>
+					</section>
+					<section class="tab-pane" id="install">
+						<article class="description">
+							<h2>Installation</h2>
+							<p>Expected</p>	
+													
+						</article>
+					</section>
+					<section class="tab-pane" id="setting">
+						<article class="description">
+							<h2>Settings</h2>
+							<p>Expected</p>	
+							
+						</article>
+					</section>
+					<section class="tab-pane" id="design">
+						<article class="description">
+							<h2>Design</h2>
+							<p>Expected</p>
+						</article>
+					</section>
+					<section class="tab-pane" id="language">
+						<article class="description">
+							<h2>Language</h2>
+							<p>The modifier has full support for several languages. English and Russian are installed by default.<br>Below you will find instructions on how to add and translate a modifier into your language.</p>
+							<p class="alert alert-warning alert-dismissible">Depending on the version of OpenCart, the folder may be called «english» or «en-gb»</p>
+							<br>
+							<hr>
+							<h3>Admin-panel:</h3> 
+							<ul>
+								<li>Copy the file <strong class="red">«html_pro.php»</strong> from the folder:<p class="alert alert-info">admin \ language \ russian \ module \ html_pro.php</p></li>
+								<li>Paste into the folder and then transfer the contents of the file:<p class="alert alert-info">admin \ language \ YOUR_LANGUAGE \ module \ html_pro.php</p></li>
+							</ul>
+						</article>
+					</section>
+					<section class="tab-pane" id="faq">
+						<article class="description">
+							
+						</article>
+					</section>
+					<section class="tab-pane" id="changelog">
+						<article class="description">
+							<div class="time-clock"><div class="time-circle"><i class="fa fa-history" aria-hidden="true"></i></div><span>Changelog</span></div>
+							<ul class="list-unstyled timeline">
+								<li>
+									<div class="block">
+										<div class="tags">
+											<h5 class="title">
+												<span class="version">v1.0</span><span class="date">31.10.2017</span>
+											</h5>
+										</div>
+										<div class="block_content">
+											<ul class="excerpt"><li>The initial version.</li></ul>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</article>
+					</section>
+					<section class="tab-pane" id="contacts">
+						<article class="description">
+							<h4 style="margin-top:0px;">Dear Clients!</h4>
+							<p>Before you write, please read the documentation.<br>If you still have questions left, then leave your request and the developer will contact you within 48 hours.</p>
+							<noindex>
+								<div class="info-url">
+									<h4 class="">Contacts</h4>
+									<p>Also you can send your message to us on the mail or leave your message in the social network.</p>
+									<div class="row">
+										<div class="col-sm-4 text-center">
+<script type="text/javascript" language="JavaScript">
+var login  = String.fromCharCode(119, 101, 98, 95, 100, 101, 118, 101, 108, 111, 112, 109, 101, 110, 116);
+var server = String.fromCharCode(109, 97, 105, 108, 46, 117, 97);
+var email  = login+'@'+server;
+var email2 ='<i class="fa fa-envelope-o"></i>'+login+'@'+server;
+var url = 'mailto:'+email;
+document.write('<a rel="nofollow" class="btn-contact" href="'+url+'">'+email2+'</a>');
+</script>
+										</div>
+										<div class="col-sm-4 text-center">
+											<a href="https://vk.com/corgras" class="btn-contact"><i class="fa fa-vk"></i>VK</a>
+										</div>
+										<div class="col-sm-4 text-center">
+											<a href="https://vk.com/studio_corgras" class="btn-contact"><i class="fa fa-vk"></i>Group VK</a>
+										</div>
+									</div>
+								</div>
+							</noindex>
+						</article>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="/assets/js/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<script src="/assets/js/stickUp.min.js"></script>
+<script src="/assets/js/script.js"></script>
+</body>
+</html>
